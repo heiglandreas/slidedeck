caabb1a1d47a05c6d6e39ea1367dacfe06b46cfe
Andreas Heigl <andreas@heigl.org>
2018-05-11 18:39:43 +0200

    Add latest changes to PDF-Talk

--- AsUserICanDownloadThisAsPdf/base/php/src/example_plop.php
+++ AsUserICanDownloadThisAsPdf/base/php/src/example_plop.php
@@ -3,10 +3,6 @@
 $plop = new Plop();
 
 $doc = $plop->open_document(__DIR__ . '/example1.pdf', '');
-$sig = $plop->prepare_signature('digitalid={filename=' . __DIR__ . '/../../cert/certificate.p12} password=demo');
-if ($sig === 0) {
-  var_Dump($plop->get_errmsg());
-  exit;
-}
+$plop->prepare_signature('digitalid={filename=' . __DIR__ . '/../../cert/certificate.p12} password=demo');
 $plop->create_document(__DIR__ . '/example_plop.pdf', 'input=' . $doc);
 $plop->close_document($doc, '');
